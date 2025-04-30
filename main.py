a7f1e7d348d0f27ddeac08ab708d8e43aa8594d1
Hanhansu <sungyo11@g.skku.edu>
2025-04-30 19:03:37 +0900

    sum_of_squares_of_even end

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def even_list(int_list: List[int]) -> List[int]:
 	pass
 
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+    	a = 0
+    	for i in even_int_list:
+        	a += i * i
+    	return a
 	pass
 
 def main():
